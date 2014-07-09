8a8efef4573c3bda605a1d65c71b3a7b94d51c20
shahed ruhi <shahedruhi@gmail.com>
2014-07-09 15:33:29 +0600

    foreach loop
    Signed-off-by: shahed ruhi <shahedruhi@gmail.com>

--- forloop.php
+++ forloop.php
@@ -1,6 +1,8 @@
 
 <?php
 
+
+/*
     $total = 0;
 	$startnumber = 5;
 	$endnumber = 6;
@@ -12,7 +14,26 @@ for($sum = $startnumber; $sum <= $endnumber; $sum++)
    }
 	
 	echo "addition is : $total";
+*/
 
+//for each loop
+
+$cars = array('toyota', 'sedan', 'mercidise');
+$fruits = array("t"=>'test', "a"=>'appale',"c"=>'cat');
+
+/*for( $i=0; $i < count($cars), $i++){
+
+	echo $cars[$i]."<br />";	
+	
+	}
+	*/
+//foreach($fruits as $value)
+foreach($fruits as $key=>$value)
+{	
+	echo $key.'--'.$value."<br />";
+	
+}
+	
  
 ?>
 
