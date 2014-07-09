33b5100e11556a54aa4d6f3ae2a23ff6b3380e85
shahedruhi <shahedruhi@gmail.com>
2014-07-09 20:01:50 +0600

    array_chunk

--- arrayfunctions.php
+++ arrayfunctions.php
@@ -2,9 +2,16 @@
 
 <?php
 
-//array_change_key_case
+/*array_change_key_case
 $input_array = array("FirSt" => 1, "SecOnd" => 4);
 print_r(array_change_key_case($input_array, CASE_UPPER));
+*/
+
+//array_chunk
+$input_array = array('a', 'b', 'c', 'd', 'e');
+print_r(array_chunk($input_array, 2));
+print_r(array_chunk($input_array, 2, true));
+
 
 
 ?>
