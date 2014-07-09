58b2ae73923d8867233161b72cd3b4f318a7ba22
shahedruhi <shahedruhi@gmail.com>
2014-07-09 20:05:23 +0600

    array_column

--- arrayfunctions.php
+++ arrayfunctions.php
@@ -8,11 +8,37 @@ print_r(array_change_key_case($input_array, CASE_UPPER));
 */
 
 //array_chunk
-$input_array = array('a', 'b', 'c', 'd', 'e');
-print_r(array_chunk($input_array, 2));
-print_r(array_chunk($input_array, 2, true));
+//$input_array = array('a', 'b', 'c', 'd', 'e');
+//print_r(array_chunk($input_array, 2));
+//print_r(array_chunk($input_array, 2, true));
 
-
+//array_column
+// Array representing a possible record set returned from a database
+$records = array(
+    array(
+        'id' => 2135,
+        'first_name' => 'John',
+        'last_name' => 'Doe',
+    ),
+    array(
+        'id' => 3245,
+        'first_name' => 'Sally',
+        'last_name' => 'Smith',
+    ),
+    array(
+        'id' => 5342,
+        'first_name' => 'Jane',
+        'last_name' => 'Jones',
+    ),
+    array(
+        'id' => 5623,
+        'first_name' => 'Peter',
+        'last_name' => 'Doe',
+    )
+);
+ 
+$first_names = array_column($records, 'first_name');
+print_r($first_names); 
 
 ?>
 
