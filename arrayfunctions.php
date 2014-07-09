d9784bc4bd29ba9270f6d05bd755af9e75b4c126
shahedruhi <shahedruhi@gmail.com>
2014-07-09 21:03:10 +0600

    13. array_filter 1

--- arrayfunctions.php
+++ arrayfunctions.php
@@ -107,10 +107,31 @@ var_dump(array_diff_ukey($array1, $array2, 'key_compare_func'));
 //print_r($a);
 
 //12. array_fill
-$a = array_fill(5, 6, 'banana');
-$b = array_fill(-2, 4, 'pear');
-print_r($a);
-print_r($b);
+//$a = array_fill(5, 6, 'banana');
+//$b = array_fill(-2, 4, 'pear');
+//print_r($a);
+//print_r($b);
+
+//13. array_filter 1
+function odd($var)
+{
+    // returns whether the input integer is odd
+    return($var & 1);
+}
+
+function even($var)
+{
+    // returns whether the input integer is even
+    return(!($var & 1));
+}
+
+$array1 = array("a"=>1, "b"=>2, "c"=>3, "d"=>4, "e"=>5);
+$array2 = array(6, 7, 8, 9, 10, 11, 12);
+
+echo "Odd :\n";
+print_r(array_filter($array1, "odd"));
+echo "Even:\n";
+print_r(array_filter($array2, "even"));
 
 ?>
 
