a31da44433ab9e972ae16f683d10926f22a0753a
shahedruhi <shahedruhi@gmail.com>
2014-07-09 20:23:50 +0600

    8. array_diff_uassoc

--- arrayfunctions.php
+++ arrayfunctions.php
@@ -58,10 +58,24 @@ print_r($first_names);
 //print_r($result);
 
 //7. array_diff_key
-$array1 = array('blue'  => 1, 'red'  => 2, 'green'  => 3, 'purple' => 4);
-$array2 = array('green' => 5, 'blue' => 6, 'yellow' => 7, 'cyan'   => 8);
+//$array1 = array('blue'  => 1, 'red'  => 2, 'green'  => 3, 'purple' => 4);
+//$array2 = array('green' => 5, 'blue' => 6, 'yellow' => 7, 'cyan'   => 8);
 
-var_dump(array_diff_key($array1, $array2));
+//var_dump(array_diff_key($array1, $array2));
+
+//8. array_diff_uassoc
+function key_compare_func($a, $b)
+{
+    if ($a === $b) {
+        return 0;
+    }
+    return ($a > $b)? 1:-1;
+}
+
+$array1 = array("a" => "green", "b" => "brown", "c" => "blue", "red");
+$array2 = array("a" => "green", "yellow", "red");
+$result = array_diff_uassoc($array1, $array2, "key_compare_func");
+print_r($result);
 
 
 
