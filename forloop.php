8175018411f39fb9c306266d5450b35c1599d354
shahed ruhi <shahedruhi@gmail.com>
2014-07-09 15:09:48 +0600

    loop

---
+++ forloop.php
@@ -0,0 +1,18 @@
+
+<?php
+
+    $total = 0;
+	$startnumber = 5;
+	$endnumber = 6;
+	
+	
+for($sum = $startnumber; $sum <= $endnumber; $sum++)
+   {
+    $total = $total + $sum;
+   }
+	
+	echo "addition is : $total";
+
+ 
+?>
+
