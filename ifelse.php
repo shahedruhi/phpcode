c408a00f3cfc4a9b851abb33105239e95b9ed01b
shahed ruhi <shahedruhi@gmail.com>
2014-07-09 14:41:40 +0600

    for 100< number< 0

--- ifelse.php
+++ ifelse.php
@@ -1,8 +1,12 @@
 
 <?php
 
-$mark = 13;
+$mark = -56;
 
+
+if($mark  >= 0 && $mark  <= 100)
+
+{
 if($mark  >= 80)
     echo "Your final grade for the course will be: A+ <br />";
 elseif ($mark  >= 70)
@@ -17,6 +21,27 @@ elseif ($mark  >= 33)
     echo "Your final grade for the course will be: D  <br />";
 else
     echo "Your final grade for the course will be: F  <br />";
+}
+
+else
+	echo "put the right number   <br />";
+	
+	
+/*if($mark  >= 80)
+    echo "Your final grade for the course will be: A+ <br />";
+elseif ($mark  > 69 && $mark  < 7)
+    echo "Your final grade for the course will be: A  <br />";
+elseif ($mark  >= 60)
+    echo "Your final grade for the course will be: A-  <br />";
+elseif ($mark  >= 50)
+    echo "Your final grade for the course will be: B  <br />";
+elseif ($mark  >= 40)
+    echo "Your final grade for the course will be: C  <br />";
+elseif ($mark  >= 33)
+    echo "Your final grade for the course will be: D  <br />";
+else
+    echo "Your final grade for the course will be: F  <br />";
+*/
 
 
 ?>
