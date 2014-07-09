dd3e5e1ae44277024ea0ea5fe74068a9b5f3f6b5
shahed ruhi <shahedruhi@gmail.com>
2014-07-09 10:02:37 +0600

    change 10.02 9-7-2014

--- index.php
+++ index.php
@@ -11,21 +11,33 @@ $a = 1;
 //block
 //nested
 
-{
-	{
-	}
-}
+//{
+//	{
+//	}
+//}
 
-//Data Type
+//Data Type | type casting
 
 $_aNumber = 1234;
-echo gettype($_aNumber);
-echo "<br>";
-echo settype($_aNumber, 'String');
-echo "<br>";
-echo gettype($_aNumber);
+//echo gettype($_aNumber);
+//echo "<br>";
+//echo settype($_aNumber, 'String');
+//echo "<br>";
+//echo gettype($_aNumber);
 
 //$_aString = abcd; 
+//$_aString = 'abcd$aNumber';
+//$_anotherString = "abcd$aNumber";
+//echo gettype($_aString);
+//echo gettype($_anotherString);
+
+//echo $_aString;
+//echo "<br>";
+//echo $_anotherString;
+//echo "<br>";
+
+$aFlotedNumber = 12.12;
+echo gettype($aFlotedNumber);
 
 
 
