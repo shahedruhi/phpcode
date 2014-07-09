bdaf30662b191aa7721e96a4dabfb07d75867bfc
shahed ruhi <shahedruhi@gmail.com>
2014-07-09 14:15:24 +0600

    ifelse

---
+++ ifelse.php
@@ -0,0 +1,23 @@
+
+<?php
+
+$mark = 13;
+
+if($mark  >= 80)
+    echo "Your final grade for the course will be: A+ <br />";
+elseif ($mark  >= 70)
+    echo "Your final grade for the course will be: A  <br />";
+elseif ($mark  >= 60)
+    echo "Your final grade for the course will be: A-  <br />";
+elseif ($mark  >= 50)
+    echo "Your final grade for the course will be: B  <br />";
+elseif ($mark  >= 40)
+    echo "Your final grade for the course will be: C  <br />";
+elseif ($mark  >= 33)
+    echo "Your final grade for the course will be: D  <br />";
+else
+    echo "Your final grade for the course will be: F  <br />";
+
+
+?>
+
--- new.php
+++ new.php
@@ -1,69 +1,28 @@
 
 <?php
-/*
-//echo "Hello FTFL";
+if ($grade > 90.9)
+    echo "Your final grade for the course will be: A+ <br />";
+elseif ($grade > 85.9)
+    echo "Your final grade for the course will be: A  <br />";
+elseif ($grade > 79)
+    echo "Your final grade for the course will be: A-  <br />";
+elseif ($grade > 76.9)
+    echo "Your final grade for the course will be: B+  <br />";
+elseif ($grade >71.9)
+    echo "Your final grade for the course will be: B  <br />";
+elseif ($grade > 69.9)
+    echo "Your final grade for the course will be: B-  <br />";
+elseif ($grade > 65.9)
+    echo "Your final grade for the course will be: C+  <br />";
+elseif ($grade > 59.9)
+    echo "Your final grade for the course will be: C  <br />";
+elseif ($grade > 54.9)
+    echo "Your final grade for the course will be: C-  <br />";
+elseif ($grade > 49.9)
+    echo "Your final grade for the course will be: P  <br />";
+else
+    echo "Your final grade for the course will be: F  <br />";
 
-//Syntax, Exercise
-
-//variables
-
-$a = 1;
-
-//block
-//nested
-
-//{
-//	{
-//	}
-//}
-
-//Data Type | type casting
-
-$_aNumber = 1234;
-//echo gettype($_aNumber);
-//echo "<br>";
-//echo settype($_aNumber, 'String');
-//echo "<br>";
-//echo gettype($_aNumber);
-
-//$_aString = abcd; 
-//$_aString = 'abcd$aNumber';
-//$_anotherString = "abcd$aNumber";
-//echo gettype($_aString);
-//echo gettype($_anotherString);
-
-//echo $_aString;
-//echo "<br>";
-//echo $_anotherString;
-//echo "<br>";
-
-$aFlotedNumber = 12.12;
-//echo gettype($aFlotedNumber);
-
-$_aBool = true;
-$x=true;
-$y=false;
- 
-$a = 2*3+5;
-//echo $a."<br />".$a;
-//echo nl2br($a."\n".$a);
-
-$name = 'S.M. Shahed Ruhi'; 
-for ($i=1; $i<=5; $i++)
-{
-//echo $name, "<br>";
-//echo "<br>";
-}
-
-$tr = array("1","2","3");
-sort($tr);
-
-$l=count($tr);
-for($x=0;$x<$l;$x++)
-   {
-   //echo $tr[$x], "<br>";
-   }
-
-*/
+} 
 ?>
 
