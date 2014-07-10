7cd5d5fcfa8033c62c2d617e7bd271b85bc184c0
shahed ruhi <shahedruhi@gmail.com>
2014-07-10 08:12:33 +0600

    addclashes & addslashes

--- stringfunctions.php
+++ stringfunctions.php
@@ -2,14 +2,19 @@
 <?php
 //1. addcslashes
 
-echo addcslashes('foo[ ]', 'A..z');
+//echo addcslashes('foo[ ]', 'A..z');
 // output:  \f\o\o\[ \]
 // All upper and lower-case letters will be escaped
 // ... but so will the [\]^_`
 //echo addcslashes("zoo['.']", 'z..A');
 // output:  \zoo['\.']
 
+//2. addslashes
 
+$str = "Is your name O'reilly?";
+
+// Outputs: Is your name O\'reilly?
+echo addslashes($str);
 
 ?>
 
