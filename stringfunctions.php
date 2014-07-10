33e9e939de57ed6a7b2965f5907514e20b65edcd
shahed ruhi <shahedruhi@gmail.com>
2014-07-10 09:00:37 +0600

    5. explode

--- stringfunctions.php
+++ stringfunctions.php
@@ -22,9 +22,27 @@
 //echo convert_uuencode($some_string);
 
 //4. crc32
-$checksum = crc32("The quick brown fox jumped over the lazy dog.");
-printf("%u\n", $checksum);
+//$checksum = crc32("The quick brown fox jumped over the lazy dog.");
+//printf("%u\n", $checksum);
 
+//5. explode
+// Example 1
+/*$pizza  = "piece1 piece2 piece3 piece4 piece5 piece6";
+$pieces = explode(" ", $pizza);
+echo $pieces[0]; // piece1
+echo $pieces[1]; // piece2
+
+// Example 2
+$data = "foo:*:1023:1000::/home/foo:/bin/sh";
+list($user, $pass, $uid, $gid, $gecos, $home, $shell) = explode(":", $data);
+echo $user; // foo
+echo $pass; // 
+*/
+ 
+$input1 = "hello";
+$input2 = "hello,there";
+var_dump( explode( ',', $input1 ) );
+var_dump( explode( ',', $input2 ) );
 
 ?>
 
