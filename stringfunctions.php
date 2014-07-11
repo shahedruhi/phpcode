11609ef68f7b4c1cf3ea9267df76a4dff6b405b8
shahedruhi <shahedruhi@gmail.com>
2014-07-11 20:09:59 +0600

    22. print

--- stringfunctions.php
+++ stringfunctions.php
@@ -230,7 +230,7 @@ if (ord($str) == 10) {
 }
 */
 //21. parse_str
-$str = "first=value&arr[]=foo+bar&arr[]=baz";
+/*$str = "first=value&arr[]=foo+bar&arr[]=baz";
 parse_str($str);
 echo $first."<br />";  // value
 echo $arr[0]."<br />"; // foo bar
@@ -240,6 +240,44 @@ parse_str($str, $output);
 echo $output['first']."<br />";  // value
 echo $output['arr'][0]."<br />"; // foo bar
 echo $output['arr'][1]."<br />"; // baz
+*/
+//22. print
+print("Hello World");
+
+print "print() also works without parentheses.";
+
+print "This spans
+multiple lines. The newlines will be
+output as well";
+
+print "This spans\nmultiple lines. The newlines will be\noutput as well.";
+
+print "escaping characters is done \"Like this\".";
+
+// You can use variables inside a print statement
+$foo = "foobar";
+$bar = "barbaz";
+
+print "foo is $foo"; // foo is foobar
+
+// You can also use arrays
+$bar = array("value" => "foo");
+
+print "this is {$bar['value']} !"; // this is foo !
+
+// Using single quotes will print the variable name, not the value
+print 'foo is $foo'; // foo is $foo
+
+// If you are not using any other characters, you can just print variables
+print $foo;          // foobar
+
+print <<<END
+This uses the "here document" syntax to output
+multiple lines with $variable interpolation. Note
+that the here document terminator must appear on a
+line with just a semicolon no extra whitespace!
+END;
+
 
 ?>
 
