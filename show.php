4806b468c1f8a5be05c3651281fd94e2f230a9ab
shahedruhi <shahedruhi@gmail.com>
2014-07-11 23:29:53 +0600

    input into database

--- show.php
+++ show.php
@@ -1,5 +1,12 @@
 <?php
 
-echo $_POST['batch_name'];
+//echo $_POST['batch_name'];
+$batch = $_POST['batch_name'];
+echo $batch;
+mysql_connect('localhost','root','');
+
+mysql_select_db('ftfl_info');
+
+mysql_query("INSERT into student_info (batch) values ('$batch')");
 
 ?>
