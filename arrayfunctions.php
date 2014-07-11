2f5fe1a6a48f7a999f96c5622bab8fca741c2ccf
shahedruhi <shahedruhi@gmail.com>
2014-07-11 17:25:09 +0600

    41. array_udiff_uassoc
    Signed-off-by: shahedruhi <shahedruhi@gmail.com>

--- arrayfunctions.php
+++ arrayfunctions.php
@@ -429,7 +429,7 @@ $b = array("a" => 1.2, "b" => 2.3, "c" => 3.4);
 echo "sum(b) = " . array_sum($b) . "\n";
 */
 //40. array_udiff_assoc
-class cr {
+/*class cr {
     private $priv_member;
     function cr($val)
     {
@@ -448,6 +448,32 @@ $b = array("0.2" => new cr(9), "0.5" => new cr(22), 0 => new cr(3), 1=> new cr(4
 
 $result = array_udiff_assoc($a, $b, array("cr", "comp_func_cr"));
 print_r($result);
+*/
+//41. array_udiff_uassoc
 
+class cr {
+    private $priv_member;
+    function cr($val)
+    {
+        $this->priv_member = $val;
+    }
+
+    static function comp_func_cr($a, $b)
+    {
+        if ($a->priv_member === $b->priv_member) return 0;
+        return ($a->priv_member > $b->priv_member)? 1:-1;
+    }
+
+    static function comp_func_key($a, $b)
+    {
+        if ($a === $b) return 0;
+        return ($a > $b)? 1:-1;
+    }
+}
+$a = array("0.1" => new cr(9), "0.5" => new cr(12), 0 => new cr(23), 1=> new cr(4), 2 => new cr(-15),);
+$b = array("0.2" => new cr(9), "0.5" => new cr(22), 0 => new cr(3), 1=> new cr(4), 2 => new cr(-15),);
+
+$result = array_udiff_uassoc($a, $b, array("cr", "comp_func_cr"), array("cr", "comp_func_key"));
+print_r($result);
 ?>
 
