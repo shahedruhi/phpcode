e897da03fa915a6e8738c117f147d1a075e210d6
shahedruhi <shahedruhi@gmail.com>
2014-07-11 22:10:20 +0600

    50. strpos

--- stringfunctions.php
+++ stringfunctions.php
@@ -521,14 +521,28 @@ usort($arr2, "strnatcmp");
 print_r($arr2);
 */
 //49. strpbrk
-$text = 'This is a Simple text.';
+/*$text = 'This is a Simple text.';
 
 // this echoes "is is a Simple text." because 'i' is matched first
 echo strpbrk($text, 'mi');
 
 // this echoes "Simple text." because chars are case sensitive
 echo strpbrk($text, 'S');
+*/
 
+//50. strpos
 
+$mystring = 'abc';
+$findme   = 'a';
+$pos = strpos($mystring, $findme);
+
+// Note our use of ===.  Simply == would not work as expected
+// because the position of 'a' was the 0th (first) character.
+if ($pos === false) {
+    echo "The string '$findme' was not found in the string '$mystring'";
+} else {
+    echo "The string '$findme' was found in the string '$mystring'";
+    echo " and exists at position $pos";
+}
 ?>
 
