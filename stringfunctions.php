0b822571a5347fa8d7ab1337194da1ec44730077
shahedruhi <shahedruhi@gmail.com>
2014-07-11 21:59:12 +0600

    44. stripos

--- stringfunctions.php
+++ stringfunctions.php
@@ -466,13 +466,31 @@ var_dump($c);
 var_dump($d);
 */
 //43. strip_tags
-$text = '<p>Test paragraph.</p><!-- Comment --> <a href="#fragment">Other text</a>';
+/*$text = '<p>Test paragraph.</p><!-- Comment --> <a href="#fragment">Other text</a>';
 echo strip_tags($text);
 echo "\n";
 
 // Allow <p> and <a>
 echo strip_tags($text, '<p><a>');
+*/
+//44. stripos
+$findme    = 'a';
+$mystring1 = 'xyz';
+$mystring2 = 'ABC';
 
+$pos1 = stripos($mystring1, $findme);
+$pos2 = stripos($mystring2, $findme);
+
+// Nope, 'a' is certainly not in 'xyz'
+if ($pos1 === false) {
+    echo "The string '$findme' was not found in the string '$mystring1'";
+}
+
+// Note our use of ===.  Simply == would not work as expected
+// because the position of 'a' is the 0th (first) character.
+if ($pos2 !== false) {
+    echo "We found '$findme' in '$mystring2' at position $pos2";
+}
 
 ?>
 
