fc4beb67c1d4e35b73b46ac277dfa65488dedd96
shahedruhi <shahedruhi@gmail.com>
2014-07-11 15:01:28 +0600

    trying to solve film exercise

--- filmtitleexercise.php
+++ filmtitleexercise.php
@@ -1,12 +1,7 @@
 
 <?php
-/*
-* To change this license header, choose License Headers in Project Properties.
-* To change this template file, choose Tools | Templates
-* and open the template in the editor.
 
-
-function getTitle ($genree="")
+/*function getTitle ($genree="")
 {
     $keyValue = 0;
     
@@ -82,86 +77,106 @@ function printTitleAndStar()
 }
 
 printTitleAndStar();
-
-
 */
-/*function find_Filmtitle_Star($film, $genere)
+
+
+function find_Film_Title($function_films, $function_genere = "")
 {
 
-$film_title = '';
-$film_star = '';
-//$film_details = '';
+$film_title = "";
 
-
-foreach($film as $key => $value)
-{	
-	if ($value  == $genere ){
-	//echo $key."<br />";
-	//echo $genere."<br />";
-	//echo $value;
-	//echo 'in'."<br />";
-	//}
-	
-	//echo $key.'--'.$value."<br />";
-	//if ($genere == $key)
-	//if ($film[genere] == $genere)
-	//{
-	$film_title = $film[film_titles][$value];
-	$film_star = $film[stars][$value];
-	$film_details = array("$film[film_titles][$value]", "$film[stars][$value]");
-	
-	}
-	
-
-echo $film_title.$film_star;	
-//return $film_title, $film_star;
-//return $film_details;
-
-}
+$index = 0;
+    
+foreach ($function_films as $key_1 => $Value_1) {
+    
+    if($key_1=="genre")
+    {
+        foreach ($Value_1 as $key_2 => $Value_2)
+        {
+            if($Value_2 == $function_genere)
+            {
+               $index = $key_2;
+               
+            }
+            
+        }
+    }
+  
 }
 
-$input_genere = "comedy";
-$film = array
+$film_title = $function_films['film_titles'][$index];
+return $film_title;
+
+}
+
+
+function find_Film_Star($function_films, $function_genere = "")
+{
+
+$film_star = "";
+
+$index = 0;
+    
+foreach ($function_films as $key_1 => $Value_1) {
+    
+    if($key_1=="genre")
+    {
+        foreach ($Value_1 as $key_2 => $Value_2)
+        {
+            if($Value_2 == $function_genere)
+            {
+               $index = $key_2;
+               
+            }
+            
+        }
+    }
+  
+}
+
+$film_star = $function_films['stars'][$index];
+return $film_star;
+
+}
+
+
+$films = array
 		(
 			"genre" => array("comedy", "tragedy", "action", "romantic"),
-			"film_titles" => array("Big", "Star Wars", "Titanic", "French Kiss"),
+			"film_titles" => array("Big", "Titanic", "Star Wars", "French Kiss"),
 			"stars" => array("Bill Murray", "Mark Hammell", "Leonardo Dicaptio", "Cate Blanechett"),
-		);		
-		
-$result = find_Filmtitle_Star($film, $input_genere);
+		);
+
+$input_genere = "comedy";
+
+/*foreach ($function_films as $key_1 => $Value_1) {
+    
+    if($key_1=="genre")
+    {
+        foreach ($Value_1 as $key_2 => $Value_2)
+        {
+            if($Value_2 == $function_genere)
+            {
+               $index = $key_2;
+               
+            }
+            
+        }
+    }
+  
+}*/
+
+
+
+$Film_Title = find_Film_Title($films, $input_genere);
+$Film_Star = find_Film_Star($films, $input_genere);
+
+echo "Film Title is : ".$Film_Title;
+echo "<br/>";
+echo "The Star Name is : ".$Film_Star;
+
+
 //print_r($film); 
-*/
-
-/*
-function find_film_genres($films) {
-foreach($films as $name => $value) {
-foreach($films[$name] as $key => $value2) {
-//echo $films['genres'][0];
-echo "<b>Genres: </b>".$films['genres'][$key];
-echo "<br />";
-
-echo "<b>Film-Title: </b>".$films['film-titles'][$key];
-echo "<br />";
-
-echo "<b>Actor/Actress: </b>".str_replace(" ", "-",$films['stars'][$key]);
-echo "<br />";
-echo "<br /><br />";
-}
-break;
-}
-}
-
-$films =	array("genres" => array("comedy","tragedy","action","romance"),
-"film-titles" => array("big","star wars", "titanic", "french kiss"),
-"stars" => array("bill murray", "mark hamett", "leonardo dicaprio", "cate blanchet"),
-);
-
-
-
-find_film_genres($films);
-
-?>
-*/
 
 
 
