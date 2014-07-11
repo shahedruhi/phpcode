d2b54ea2896276986d2f4da35b6821dc9d13559c
shahedruhi <shahedruhi@gmail.com>
2014-07-11 17:11:39 +0600

    40. array_udiff_assoc

--- arrayfunctions.php
+++ arrayfunctions.php
@@ -422,12 +422,32 @@ print_r($input);
 echo "<br >";
 */
 //39. array_sum
-$a = array(2, 4, 6, 8);
+/*$a = array(2, 4, 6, 8);
 echo "sum(a) = " . array_sum($a) . "\n";
 
 $b = array("a" => 1.2, "b" => 2.3, "c" => 3.4);
 echo "sum(b) = " . array_sum($b) . "\n";
+*/
+//40. array_udiff_assoc
+class cr {
+    private $priv_member;
+    function cr($val)
+    {
+        $this->priv_member = $val;
+    }
 
+    static function comp_func_cr($a, $b)
+    {
+        if ($a->priv_member === $b->priv_member) return 0;
+        return ($a->priv_member > $b->priv_member)? 1:-1;
+    }
+}
+
+$a = array("0.1" => new cr(9), "0.5" => new cr(12), 0 => new cr(23), 1=> new cr(4), 2 => new cr(-15),);
+$b = array("0.2" => new cr(9), "0.5" => new cr(22), 0 => new cr(3), 1=> new cr(4), 2 => new cr(-15),);
+
+$result = array_udiff_assoc($a, $b, array("cr", "comp_func_cr"));
+print_r($result);
 
 ?>
 
