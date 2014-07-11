9a7499659de3ad18311a57068717e6b77429c7df
shahedruhi <shahedruhi@gmail.com>
2014-07-11 13:57:21 +0600

    exercise complete

--- triangleexercise.php
+++ triangleexercise.php
@@ -1,11 +1,14 @@
 <?php
 
-    for($i=1;$i<=5;$i++){
-        for($j=1;$j<=$i;$j++)
-        {
-                    echo "*";
-        }
-        echo "<br />";
+echo "<pre>"; // This is for correct handling of newlines
+$input_char = "*"; // take * 
+$level = 5; // level of triangle
+$level = $level * 2;
+for($i = 1; $i <= $level; $i ++) 
+    {    	
+        if (!($i % 2) && $i != 1)
+            continue;  
+        print str_pad(str_repeat($input_char, $i),($level - 1) * strlen($input_char), " " , STR_PAD_BOTH);
+        print PHP_EOL;
     }
-
 ?>
