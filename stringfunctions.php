f3122c9877071316f125974d633498bad0c30e21
shahedruhi <shahedruhi@gmail.com>
2014-07-12 20:17:00 +0600

    //69. vsprintf

--- stringfunctions.php
+++ stringfunctions.php
@@ -735,7 +735,10 @@ $bar = ucwords(strtolower($bar)); // Hello World!
 echo $bar."<br />";
 */
 //68. vprintf
-vprintf("%04d-%02d-%02d", explode('-', '1988-8-1')); // 1988-08-01
+//vprintf("%04d-%02d-%02d", explode('-', '1988-8-1')); // 1988-08-01
+
+//69. vsprintf
+print vsprintf("%04d-%02d-%02d", explode('-', '1988-8-1')); // 1988-08-01
 
 
 
