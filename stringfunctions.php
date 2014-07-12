0a1cbff365c5ba8f9e002a4c69a5af2160c0c81d
shahedruhi <shahedruhi@gmail.com>
2014-07-12 19:30:34 +0600

    62. substr_count

--- stringfunctions.php
+++ stringfunctions.php
@@ -633,13 +633,34 @@ echo strtr("baab", $trans);
 */
 
 //61. substr_compare
-echo substr_compare("abcde", "bc", 1, 2); // 0
+/*echo substr_compare("abcde", "bc", 1, 2); // 0
 echo substr_compare("abcde", "de", -2, 2); // 0
 echo substr_compare("abcde", "bcg", 1, 2); // 0
 echo substr_compare("abcde", "BC", 1, 2, true); // 0
 echo substr_compare("abcde", "bc", 1, 3); // 1
 echo substr_compare("abcde", "cd", 1, 2); // -1
 echo substr_compare("abcde", "abc", 5, 1); // warning
+*/
+//62. substr_count
+$text = 'This is a test';
+echo strlen($text); // 14
+
+echo substr_count($text, 'is'); // 2
+
+// the string is reduced to 's is a test', so it prints 1
+echo substr_count($text, 'is', 3);
+
+// the text is reduced to 's i', so it prints 0
+echo substr_count($text, 'is', 3, 3);
+
+// generates a warning because 5+10 > 14
+echo substr_count($text, 'is', 5, 10);
+
+
+// prints only 1, because it doesn't count overlapped substrings
+$text2 = 'gcdgcdgcd';
+echo substr_count($text2, 'gcdgcd');
+
 
 ?>
 
