b0c06a7eb55f6a4037993b9413af25afd09c06cb
shahedruhi <shahedruhi@gmail.com>
2014-07-12 21:17:36 +0600

    some edit & testing

--- show.php
+++ show.php
@@ -11,20 +11,20 @@ if (strlen($batch) < 4)
 {
 	header('location:add_batch.php?len=short');
 }
-elseif (strlen($batch) > 6) 
+elseif (strlen($batch) > 7) 
 {
 	header('location:add_batch.php?len=long');
 }
 
-//echo str_replace(array('(',')','{','}','[',']'), '', $batch);
-echo "O".$batch;
-/*
+$n_batch = str_replace(array('(',')','{','}','[',']'), '', $batch);
+//echo "O".$batch;
+
 
 
 mysql_connect('localhost','root','');
 
 mysql_select_db('ftfl_info');
 
-mysql_query("INSERT into student_info (batch) values ('$batch')");
-*/
+mysql_query("INSERT into student_info (batch) values ('$n_batch')");
+
 ?>
