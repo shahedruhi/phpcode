fd69f6597aa621d97b83916ce10021d5f5fb515e
shahed ruhi <shahedruhi@gmail.com>
2014-07-13 12:28:13 +0600

    Simple interest calculator

---
+++ calculator.php
@@ -0,0 +1,29 @@
+
+<html>
+<head>
+	<title>Simple Interest Calculator</title>
+</head>
+<body>
+	<form action = "calculator.php" method = "GET">
+		<input type = 'text' name = 'pricipalAmountText'> <br />
+		<input type = 'text' name = 'annualInterestText'> <br />
+		<input type = 'text' name = 'timePeriodText'> <br />
+		<input type = "submit" name = 'submitButtion'> <br />
+	</form>	
+	<?php
+		require 'interestcalculator.php'; 
+		if(isset($_GET['submitButtion']))
+		{
+			$an_input = new InterestCalculator ();
+			$an_input->principal_Amount = $_GET['pricipalAmountText'];
+			$an_input->annual_interest_rate = $_GET['annualInterestText'];
+			$an_input->time_period = $_GET['timePeriodText'];
+
+			$total_amount = $an_input->get_total_amount(); 
+
+			echo $an_input->principal_Amount. ' '. $an_input->annual_interest_rate. ' '. $an_input->time_period. ' Result'.' '. $total_amount;
+		}
+	?>
+
+</body>
+</html>	
