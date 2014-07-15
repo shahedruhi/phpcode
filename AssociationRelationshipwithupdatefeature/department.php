504d0e0f930535882124f8f90c1a6df32fada0f1
shahedruhi <shahedruhi@gmail.com>
2014-07-15 20:39:07 +0600

    try to add update feature

--- AssociationRelationshipwithupdatefeature/department.php
+++ AssociationRelationshipwithupdatefeature/department.php
@@ -5,6 +5,7 @@ class Department {
     private $code;
     private $name;
     private $student_list;
+    private $MAX_NO_OF_STUDENTS = 5;
     
     function __construct($code, $name) {
         $this->code = $code;
@@ -12,12 +13,45 @@ class Department {
         $this->student_list = array();
     }
 
-    public function getCode() {
+    public function get_code() {
         return $this->code;
     }
 
-    public function getName() {
+    public function get_name() {
         return $this->name;
+    }
+    
+    public function get_no_of_students() {
+        return count($this->student_list);
+    }
+    
+    public function get_all_students()
+    {
+        return $this->student_list;
+    }
+
+    public function add_student($a_student)
+    {
+        
+        if (count($this->student_list) < $this->MAX_NO_OF_STUDENTS)
+            {
+                
+                foreach ($this->student_list as $student)
+                {
+                    if($a_student->get_reg_no() == $student->get_reg_no())
+                    {
+                        return "Already Exists";
+                    }
+
+                }
+                $this->student_list[] = $a_student;
+                return 'Student added';
+            }
+        else
+        {
+            return 'No vacancy';
+        }
+            
     }
 
 
