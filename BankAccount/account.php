5440ad621abb9a89f73e41b619ee6932ab90f636
shahed ruhi <shahedruhi@gmail.com>
2014-07-15 10:48:16 +0600

    oop Day 3 Bank account shiow in same page complete

--- BankAccount/account.php
+++ BankAccount/account.php
@@ -1,56 +1,55 @@
-
 <?php
 
- //declaring a class
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of account
+ *
+ * @author LAB
+ */
 class Account {
-	
-	private $name;
-	private $number;
-	private $balance;
+    
+    private $account_number;
+    private $opening_date;
+    private $balance;
+    
+    
+    public function deposit($amount)
+    {
+        $this->balance += $amount;
+        return 'Deposited';
+    }
+    function __construct($account_number, $opening_date) {
+        $this->account_number = $account_number;
+        $this->opening_date = $opening_date;
+        $this->balance = 0;
+    }
+    public function get_account_number() {
+        return $this->account_number;
+    }
 
-	public function get_name()
-	{
-		return $this->name;
-	}
+    public function get_opening_date() {
+        return $this->opening_date;
+    }
 
-	public function get_number()
-	{
-		return $this->number;
-	}
+    public function get_balance() {
+        return $this->balance;
+    }
 
-	public function get_balance()
-	{
-		return $this->balance;
-	}
-
-	function __construct($name, $number)
-	{
-		$this->name = $name;
-		$this->number = $number;
-		$this->balance = 0;
-		
-	}
-
-	public function deposit($amount)
-	{
-		$this->balance = $this->balance + $amount;
-	}
-
-	public function withdraw($amount)
-	{
-		if ($this->balance - $amount >= 0)
-		{
-			$this->balance = $this->balance - $amount;
-			return "withdrawn";
-		}
-		else 
-		{
-			return "Insufficient Balance";	
-		}
-
-		
-	}
-
-
+        public function withdraw($amount)
+    {
+        if($this->balance - $amount >=0)
+        {
+            $this->balance -= $amount;
+            return "withdrawn";
+        }
+        else {
+            return "Insufficient Balance";
+        }
+    }
+    //put your code here
 }
-?>
--- BankAccount/index.php
+++ BankAccount/index.php
@@ -1,76 +1,102 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
 <html>
-<head>
-	<title>Account Tranasction</title>
-</head>
-<body>
-	<form>
-		<table border="1" style="width:300px">
-			<tr>
-				<td>Customer Name: </td>
-				<td><input type = "text" name = 'customerNameText'> <br /></td>
-			</tr>
-			<tr>
-				<td>Account No: </td>
-				<td><input type = "text" name = 'accountNumberText'> <br /></td>
-			</tr>
-		    <tr>
-		    	<td> </td>	
-				<td><input type = "submit" value = "Create" name = 'createButtion'><br /></td>
-		    </tr>
-		     <tr>
-		    	<td> </td>	
-				<td>Amount: <input type = "text" name = 'amountText'><br /></td>
-		    </tr>
-		     <tr>
-		    	<td> </td>	
-				<td><input type = "submit" value = "Deposite" name = 'depositeButtion'><br /></td>
-		    </tr>
-		     <tr>
-		    	<td> </td>	
-				<td><input type = "submit" value = "Withdraw" name = 'withdrawButtion'><br /></td>
-		    </tr>
-		    <tr>
-		    	<td> </td>	
-				<td><input type = "submit" value = "Show Report" name = 'showreportButtion'><br /></td>
-		    </tr>
-		</table>		
-	</form>	
-	<?php
-		require_once 'account.php';
-		session_start();
+    <head>
+        <meta charset="UTF-8">
+        <title>Customer E-Account Info Entry</title>
+    </head>
+    <body>
+        <form>
+            <table border="1" style="width:300px">
+            <tr>
+            <td>Customer Name: </td>
+            <td><input type = "text" name = 'customerNameText'> <br /></td>
+            </tr>
+            <tr>
+            <td>Email Address:</td>
+            <td><input type = "text" name = 'emailAddressText'> <br /></td>
+            </tr>
+            <tr>
+            <td>Account Number:</td>
+            <td><input type = "text" name = 'accountNumberText'> <br /></td>
+            </tr>
+            <tr>
+            <td>Opening Date:</td>
+            <td><input type = "text" name = 'openingDateText'> <br /></td>
+            </tr>
+            <tr>
+            <td> </td>	
+            <td><input type = "submit" value = "Create Account" name = 'createAccountButton'><br /></td>
+            </tr>
+            <tr>
+            <td>Transaction: </td>	
+            </tr>
+            <tr>
+            <td>Amount:</td>
+            <td><input type = "text" name = 'amountText'> <br /></td>
+            </tr>
+            <tr>
+            <td> </td>	
+            <td><input type = "submit" value = "Deposit" name = 'depositButton'><br /></td>
+            </tr>
+            <tr>
+            <td> </td>	
+            <td><input type = "submit" value = "Withdraw" name = 'withdrawButton'><br /></td>
+            </tr>
+            <tr>
+            <td> </td>	
+            <td><input type = "submit" value = "Show Report" name = 'showreportButton'><br /></td>
+            </tr>
+            </table>	
+        </form>	
+        <?php
+        
+       
+       require_once 'customer.php';
+       require_once 'account.php';
+               
+       session_start();
 
-		if (isset($_GET['createButtion']))
-		{
-			$account = new Account ($_GET['customerNameText'], $_GET['accountNumberText']);
-			$_SESSION['an_account'] = $account;
-			echo "Account Created";
-		}
+       if (isset($_GET['createAccountButton']))
+       {
+        $account = new Account($_GET['accountNumberText'], $_GET['openingDateText']);
+        
+        $customer = new Customer();
+        $customer->set_customer_name($_GET['customerNameText']);
+        $customer->set_customer_email_address($_GET['emailAddressText']);
+        $customer->set_customer_account($account);
+        
+        $_SESSION['a_customer'] = $customer;
+        echo "Account Created";
+       }
 
-		if (isset($_GET['depositeButtion']))
-		{
-			$account = $_SESSION['an_account'];
-			$account->deposit($_GET['amountText']);
-			$_SESSION['an_account'] = $account;
-			echo "Deposited";
-		}
-		
-		if (isset($_GET['withdrawButtion']))
-		{
-			$account = $_SESSION['an_account'];
-			echo $account->withdraw($_GET['amountText']);
-			$_SESSION['an_account'] = $account;
-		}
+       if (isset($_GET['depositButton']))
+       {
+        $customer = $_SESSION['a_customer'];
+        echo $customer->get_customer_account()->deposit($_GET['amountText'])."<br/>";
+        $_SESSION['a_customer'] = $customer;
+       }
 
-		if (isset($_GET['showreportButtion']))
-		{
-			$account = $_SESSION['an_account'];
-			echo $account->get_name(). "<br/>";
-			echo $account->get_number(). "<br/>";
-			echo $account->get_balance(). "<br/>";
-		
-		}
-				
-	?>
+       if (isset($_GET['withdrawButton']))
+       {
+        $customer = $_SESSION['a_customer'];
+        echo $customer->get_customer_account()->withdraw($_GET['amountText'])."<br/>";
+        $_SESSION['a_customer'] = $customer;         
+       }
 
-</body>
-</html>	
+       if (isset($_GET['showreportButton']))
+       {
+        $customer = $_SESSION['a_customer'];
+        echo "Customer Account No: ". $customer->get_customer_account()->get_account_number(). "<br/>";
+        echo "Customer Name: ". $customer->get_customer_name(). "<br/>";
+        echo "Opening Date: ". $customer->get_customer_account()->get_opening_date(). "<br/>";
+        echo "Balance: ". $customer->get_customer_account()->get_balance(). "<br/>";
+        }
+        
+        ?>
+    </body>
+</html>
